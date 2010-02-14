8f369be840129232dee8b837e9da9795c38975e5
John Resig <jeresig@gmail.com>
2010-02-14 14:07:53 -0500

    Improve the slideshow (links for slides, static slide height).

--- intro/slideshow.js
+++ intro/slideshow.js
@@ -7,19 +7,35 @@ jQuery(document).ready(function(){
 		text = text
 			.replace(/&lt;/g, "<").replace(/&gt;/g, ">")
 			.replace(/&nbsp;/g, " ").replace(/\s/g, " ")
-			.replace(/jQuery\("/g, "jQuery(\"#" + slide.attr("id") + " ");
+			.replace(/$\("/g, "$(\"#" + slide.attr("id") + " ");
 		eval( text );
 	}
+	
+	jQuery.fn.goto = function(){
+		var self = this;
+		
+		if ( self.length ) {
+			jQuery(document.body)
+				.animate({scrollTop: self.offset().top}, "slow", function(){
+					window.location.hash = self.find("h1").attr("id");
+				});
+		}
+		
+		return this;
+	};
 	
 	jQuery("button.run").click(analyze);
 	jQuery("pre.run").each(analyze);
 	
-	jQuery("h1").click(function(){
-		var nextSlide = jQuery(this).parent().next();
+	jQuery("h1")
+		.attr("id", function(i){ return i; })
+		.click(function(){
+			jQuery(this).parent().next().goto();
+		});
+	
+	jQuery("div.slide").height(function(i, height){ return height; });
 		
-		if ( nextSlide.length ) {
-			jQuery(document.body)
-				.animate({scrollTop: nextSlide.offset().top}, "slow");
-		}
-	});
+	if ( window.location.hash ) {
+		jQuery(window.location.hash).parent().goto();
+	}
 });
